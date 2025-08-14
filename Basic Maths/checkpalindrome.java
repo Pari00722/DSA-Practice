0718448cd099ccd5726963aa2c237e1632ad2bfe
Pari00722 <parirakesh284@gmail.com>
2025-08-14 21:10:23 +0530

    check for Palindrome

---
+++ Basic Maths/checkpalindrome.java
@@ -0,0 +1,30 @@
+public class checkpalindrome {
+    public int palindromeNumber(int n) {
+        int reverseNumber = 0;
+        int duplicateofn = n;
+        
+        while (n > 0) {
+            int lastDigit = n % 10;
+            n = n / 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+        }
+        
+        if (duplicateofn == reverseNumber) {
+            return 1; 
+        } else {
+            return 0; 
+        }
+    }
+
+    public static void main(String[] args) {
+        checkpalindrome obj = new checkpalindrome(); 
+        int n = 45654; 
+        int result = obj.palindromeNumber(n); 
+
+        if (result == 1) {
+            System.out.println(n + " is a Palindrome.");
+        } else {
+            System.out.println(n + " is NOT a Palindrome.");
+        }
+    }
+}
