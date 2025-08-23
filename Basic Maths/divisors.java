cf090b4dee7f01a51fbac358109da13ee356451b
Pari00722 <parirakesh284@gmail.com>
2025-08-23 23:33:54 +0530

    commit

--- Basic Maths/divisors.java
+++ Basic Maths/divisors.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class divisors {
     public static void main(String[] args) {
-       int n = 36;
+       int n = 124;
        List<Integer>divisors=new ArrayList<>();
        
        for(int i=1;i*i<=n;i++){
