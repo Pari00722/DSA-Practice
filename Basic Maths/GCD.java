cb00c6661943f4f6cb092e124928fbeebaf66fb1
Pari00722 <parirakesh284@gmail.com>
2025-08-24 23:29:01 +0530

    commit

--- Basic Maths/GCD.java
+++ Basic Maths/GCD.java
@@ -9,7 +9,7 @@ public class GCD {
     }
 
     public static void main(String[] args) {
-        int a = 56, b = 98;
+        int a = 197, b = 78;
         System.out.println("GCD of " + a + " and " + b + " is: " + gcd(a, b));
     }
 }
