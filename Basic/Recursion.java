916a06240e30016fe258c7bc494c2bbbe788104f
Pari00722 <parirakesh284@gmail.com>
2025-09-06 23:10:36 +0530

    recursion basic

--- Basic/Recursion.java
+++ Basic/Recursion.java
@@ -1,18 +1,15 @@
 public class recursion{
-    // Recursive function
     static void printNTimes(int n) {
         // Base case
         if (n == 0) {
             return;
         }
-
-        // Recursive step
-        printNTimes(n - 1); // call for smaller problem
-        System.out.println("Hello " + n); // print after recursion
+        printNTimes(n - 1); 
+        System.out.println("Hello " + n); 
     }
 
     public static void main(String[] args) {
-        int n = 5; // You can change this
+        int n = 9;
         printNTimes(n);
     }
 }
