cbe168da45f749cc7fde79d2f3be0c990c5ec4d2
Pari00722 <parirakesh284@gmail.com>
2025-09-13 21:52:36 +0530

    fibonacci number

---
+++ Basic/fibonacci.java
@@ -0,0 +1,14 @@
+public class fibonacci {
+    static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        int last=fib(n-1);
+        int slast=fib(n-2);
+        return last+slast;
+    }
+    public static void main(String[] args) {
+        int n =6;
+        System.out.println("Fibonacci number of "+n+"th position is "+fib(n));
+    }
+}
