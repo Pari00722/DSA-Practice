2beca3c1965b26cebb2d6fe47103da2e7b85c8eb
Pari00722 <parirakesh284@gmail.com>
2025-11-09 23:19:21 +0530

    cs

---
+++ Basic/CombinationSum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class CombinationSum{
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+
+        List<List<Integer>> ans = combinationSum(candidates, target);
+        System.out.println(ans);
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void backtrack(int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (target < 0) return;
+
+        for (int i = start; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            // stay at same index i because we can reuse same number unlimited times
+            backtrack(candidates, target - candidates[i], i, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
