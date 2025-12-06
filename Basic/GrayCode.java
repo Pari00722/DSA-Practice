f1a4a015019848798a82a42498087dba96a78ded
Pari00722 <parirakesh284@gmail.com>
2025-12-06 23:04:48 +0530

    gray code

---
+++ Basic/GrayCode.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class GrayCode {
+
+    public static List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        int size = 1 << n; // 2^n
+
+        for (int i = 0; i < size; i++) {
+            result.add(i ^ (i >> 1));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        List<Integer> code = grayCode(n);
+        System.out.println(code);
+    }
+}
